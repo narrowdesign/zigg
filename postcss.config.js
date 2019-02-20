e7d6442683402089d31de2bbbb4c73c9529635d6
narrowdesign <nick@narrowdesign.com>
2019-02-19 20:35:17 -0500

    desktop coded

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   plugins: [
-    require('precss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('postcss-functions')({
+      functions: {
+        flux(min, max, screenMin = '420px', screenMax = '1920px') {
+          const spread = parseInt(max,10) - parseInt(min,10);
+          const screenSpread = parseInt(screenMax,10) - parseInt(screenMin,10);
+          let css = `calc(${min} + ${spread} * (100vw - ${screenMin}) / ${screenSpread})`;
+          return css
+        },
+        clamp(min, max, screenMin = '420px', screenMax = '1920px') {
+          const spread = parseInt(max,10) - parseInt(min,10);
+          const screenSpread = parseInt(screenMax,10) - parseInt(screenMin,10);
+          let css = `calc(${min} + ${spread} * (100vw - ${screenMin}) / ${screenSpread})`;
+          return css
+        }
+      }
+    }),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
   ]
 }
